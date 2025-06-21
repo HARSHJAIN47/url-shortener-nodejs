f67f5bac7efbfbeac9ddaebb27b7abeb088cfa70
harsh jain <hitikjain8@gmail.com>
2025-06-22 01:34:29 +0530

    make port number env ready.

--- server.js
+++ server.js
@@ -155,7 +155,7 @@ const server = createServer(async (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
